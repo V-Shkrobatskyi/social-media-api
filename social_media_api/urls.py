216e33ec68b711bff1499178bc32b68e6eb8b32b
Vitalii <vetali5700gmail.com>
2024-07-13 22:32:09 +0300

    add urls files in each app

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/post/", include("post.urls", namespace="post")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
