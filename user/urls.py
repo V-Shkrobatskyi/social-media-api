74c8b398b4707fac63cc00fd96b70e306426d48a
Vitalii <vetali5700gmail.com>
2024-07-15 23:17:00 +0300

    add register, login, logout and me user endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
+from user.views import CreateUserView, CreateTokenView, ManageUserView, LogoutUserView
 
 app_name = "user"
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+from django.contrib.auth import logout
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = ()
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class LogoutUserView(APIView):
+    def get(self, request):
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
