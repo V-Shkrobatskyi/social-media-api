00642a08504499bf07427973e35042f8bedcd322
Vitalii <vetali5700gmail.com>
2024-07-20 22:08:03 +0300

    fix profile_image_file_path function

--- social_network/models.py
+++ social_network/models.py
@@ -7,9 +7,9 @@ from social_media_api import settings
 
 def profile_image_file_path(instance, filename):
     extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.owner)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/profile_images/", filename)
 
 
 class Profile(models.Model):
