fdf04f696017b5bb2d1bd71111c2ad8fe2e8fc18
Vitalii <vetali5700gmail.com>
2024-07-26 22:22:02 +0300

    add MEDIA_URL and MEDIA_ROOT vars, disable DEFAULT_PERMISSION_CLASSES before realise it

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from social_media_api import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +29,4 @@ urlpatterns = [
     ),
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
