93a5f2a2164455bf99697fc78de1c4bcfaa7d2d9
Vitalii <vetali5700gmail.com>
2024-08-12 22:34:32 +0300

    add drf-spectacular path

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -18,6 +18,11 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView,
+)
 
 from social_media_api import settings
 
@@ -29,4 +34,15 @@ urlpatterns = [
     ),
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
